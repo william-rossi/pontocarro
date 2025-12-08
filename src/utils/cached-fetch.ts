36d83d9157ba7b14562bd6b8b00d11a12c024bfb
William <wrsmacc@gmail.com>
2025-12-08 20:29:53 -0300

    cache

--- src/utils/cached-fetch.ts
+++ src/utils/cached-fetch.ts
@@ -1,5 +1,5 @@
 export async function cachedFetch(input: RequestInfo | URL, init?: RequestInit): Promise<Response> {
-  const revalidateTime = Number.parseInt(process.env.NEXT_PUBLIC_CACHE_REVALIDATE_MS ?? '300')
+  const revalidateTime = Number.parseInt(process.env.NEXT_PUBLIC_CACHE_REVALIDATE ?? '300')
   const defaultOptions = {
     next: { revalidate: revalidateTime },
   };
